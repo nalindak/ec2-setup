1f52bba365e169aeb557383c0d7875fc84e93e39
Nalinda <nalindakarunaratna@gmail.com>
2022-06-07 22:21:27 +1000

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Setup `ec2` Instance
+
+```
+curl -s 'https://raw.githubusercontent.com/nalindak/ec2-setup/main/install.sh' | sh
+```
--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 mv ~/.bashrc ~/.bashrc_original
-curl https://raw.githubusercontent.com/nalindak/ec2-setup/main/.bashrc -o ~/.bashrc
+curl -s 'https://raw.githubusercontent.com/nalindak/ec2-setup/main/.bashrc' -o ~/.bashrc
+source ~/.bashrc
 
 sudo yum update -y
 sudo yum install docker git jq -y
 sudo usermod -aG docker $USER
 sudo service docker start
 sudo chmod 666 /var/run/docker.sock
-
-source ~/.bashrc
