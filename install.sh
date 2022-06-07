f7e6115093c67cf62b57dca9768e5b6bfc7d3a13
Nalinda <nalindakarunaratna@gmail.com>
2022-06-07 21:52:17 +1000

    installation

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mv ~/.bashrc ~/.bashrc_original
+curl https://github.com/nalindak/ec2-setup/blob/main/.bashrc > ~/.bashrc
+source ~/.bashrc
+
 sudo yum update -y
 sudo yum install docker git jq -y
 sudo usermod -aG docker $USER
